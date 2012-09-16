b39deb3b8a0ab395421c19e2fdb10a7cf5c3ecc2
trippled <t.lang93+clan@gmail.com>
2012-09-16 16:38:20 +0200

    Testcommit

--- src/engine/server/server.h
+++ src/engine/server/server.h
@@ -1,5 +1,6 @@
 /* (c) Magnus Auvinen. See licence.txt in the root of the distribution for more information. */
 /* If you are missing that file, acquire a complete release at teeworlds.com.                */
+//Test
 #ifndef ENGINE_SERVER_SERVER_H
 #define ENGINE_SERVER_SERVER_H
 
